6b9ffc24e10118725488cfa0cf08449143bc98cf
Lorenzo Bulosan Bulosan <Lorenzo.bulosan@yahoo.co.uk>
2021-09-27 17:38:56 +0100

    saving point

--- Assets/Scripts/Background/ParallaxLayer.cs
+++ Assets/Scripts/Background/ParallaxLayer.cs
@@ -12,10 +12,18 @@ namespace Project.Scripts.Parallax
         [Tooltip("Amount of parallax. 1 simulates being close to the camera, -1 simulates being very far from the camera.")]
         private float parallaxAmount;
 
-
         [NonSerialized] private Vector3 newPosition;
 
-        // private bool adjusted = false;
+        private float lengthOfLayer;
+
+        [SerializeField]
+        private Camera cameraObj;
+
+        private void Awake()
+        {
+            cameraObj = GetComponent<Camera>();
+            
+        }
 
         public void MoveLayer(float positionChangeX, float positionChangeY)
         {
@@ -26,11 +34,26 @@ namespace Project.Scripts.Parallax
             transform1.localPosition = newPosition;
 
             // IMPLEMENT INFINITE SCROLLING
+            InfiniteScrollingHorizontal();
         }
 
         private void InfiniteScrollingHorizontal()
         {
-           
+            // get layer position of edges x0 and x1
+            var camerax0 = cameraObj.transform.position.x;
+            var camerax1 = cameraObj.orthographicSize*2f;
+
+            // get camera position of edges too c0 and c1
+
+            // when the camera edge c1 reaches x1 then move layer to start at x1, and inverse
+
+            // test move layer
+            var transform2 = transform;
+            newPosition = transform2.localPosition;
+            newPosition.x -= 0.02f;
+            transform2.localPosition = newPosition;
+
+            Console.WriteLine(newPosition);
         }
     }
 }
