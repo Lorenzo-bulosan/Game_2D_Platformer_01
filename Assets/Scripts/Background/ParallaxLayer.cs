c89da6fde1130c2e2663f837a7cec6cbf9179c23
Jimmy <jkchuong@gmail.com>
2021-10-11 21:58:03 +0100

    fucked some shit up

--- Assets/Scripts/Background/ParallaxLayer.cs
+++ Assets/Scripts/Background/ParallaxLayer.cs
@@ -15,7 +15,8 @@ namespace Project.Scripts.Parallax
         //    [NonSerialized] private Vector3 newPosition;
 
         // variablles
-        private float lengthBackgroundOneSet, startPos, distance, deltaLength;
+        private float lengthBackgroundOneSet, startPos, distance, newPostion, deltaLength;
+        private PlayerController player;
 
         // ref
         public GameObject _camera;
@@ -24,23 +25,53 @@ namespace Project.Scripts.Parallax
         private void Awake()
         {
             startPos = transform.position.x;
-            lengthBackgroundOneSet = GetComponent<SpriteRenderer>().bounds.size.x;
+            Debug.Log("Starting Position:" + startPos);
+            lengthBackgroundOneSet = GetComponentInChildren<SpriteRenderer>().bounds.size.x;
+            Debug.Log("Sprite Size: " + lengthBackgroundOneSet);
+        }
+
+        private void Start()
+        {
+            player = FindObjectOfType<PlayerController>();
         }
 
         private void Update()
         {
             // move layer faster or camera follow depending on external user input variable
-            //MoveLayerByParallaxAmount();
+            MoveLayerByParallaxAmount();
 
             // infinite scrolling, as front layers will move faster than further ones
-            InfiniteScrollingHorizontal();
+            // InfiniteScrollingHorizontal();
         }              
         
         private void MoveLayerByParallaxAmount()
         {
             // calculates parallax and move by that much each layer depending on variable given to each
             distance = (_camera.transform.position.x * parallaxEffect);
+
             transform.position = new Vector3(startPos + distance, transform.position.y, transform.position.z);
+
+            // Check if player is quarter of the way past the original position
+            float distanceFromPlayerToSnappingPoint = player.transform.position.x - startPos;
+            if (Mathf.Abs(distanceFromPlayerToSnappingPoint) >= lengthBackgroundOneSet / 2)
+            {
+                transform.position = new Vector3(transform.position.x + (lengthBackgroundOneSet * Mathf.Sign(distanceFromPlayerToSnappingPoint)), transform.position.y, transform.position.z);
+                
+                startPos = transform.position.x;
+                
+                Debug.Log(startPos);
+            }
+            
+            // Check if the total distance moved so far is equal to half the distance of the length
+            // if (Mathf.Abs(transform.position.x - startPos) >= lengthBackgroundOneSet / 2)
+            // {
+            //     // Move layer +- one whole length depending on totalDistance
+            //     transform.position = new Vector3(transform.position.x - lengthBackgroundOneSet, transform.position.y, transform.position.z);
+            //
+            //     startPos = transform.position.x;
+            // }
+            //
+            // distance = 0;
         }
 
         private void InfiniteScrollingHorizontal()
